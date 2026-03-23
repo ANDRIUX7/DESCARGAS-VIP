1477c780dc3d5b387f8da830b0d5a48138796893
ANDRIUX7 <80667506+ANDRIUX7@users.noreply.github.com>
2026-03-22 23:01:41 -0500

    Enhance login function with background GIF
    Added background GIF change upon successful login.

--- public/app.js
+++ public/app.js
@@ -4,11 +4,15 @@ function login() {
   if (pass === "ANDRIUXVIP7") {
     document.getElementById("login").style.display = "none";
     document.getElementById("app").style.display = "block";
+
+    //  CAMBIAR FONDO A GIF
+    document.body.style.background = "url('https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3lyczk5Yno1bW55Z2t2OGp5MWY0eGE2b3Ryc3c0eTQ0a2RoczVxMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3f63VcWeaGeaQ6AfKk/giphy.gif') no-repeat center center fixed";
+    document.body.style.backgroundSize = "cover";
+
   } else {
     alert("Contraseña incorrecta");
   }
 }
-
 function descargar() {
   const url = document.getElementById("url").value;
   const bar = document.getElementById("bar");
