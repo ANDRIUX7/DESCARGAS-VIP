c475449a88c2e6737a6436c4c61766256495615d
ANDRIUX7 <80667506+ANDRIUX7@users.noreply.github.com>
2026-06-29 20:00:27 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+# Salir inmediatamente si un comando falla
+set -o errexit
+
+# Instalar dependencias de Node.js
+npm install
+
+# Instalar yt-dlp usando pip de forma local para el entorno
 pip install yt-dlp
